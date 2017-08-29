f255ab5999347352382149b632a66401301216ba
collin <collinalpert@gmail.com>
2017-08-29 22:30:50 +0200

    Added JCheckBoxGroup

---
+++ javax/swing/JCheckBoxGroup.java
@@ -0,0 +1,43 @@
+package javax.swing;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class JCheckBoxGroup extends JPanel {
+
+    private ArrayList<JCheckBox> list = new ArrayList<>();
+
+    public JCheckBoxGroup(JCheckBox... checkBoxes) {
+        Arrays.stream(checkBoxes).forEach(this::add);
+        list.addAll(Arrays.asList(checkBoxes));
+    }
+
+    public JCheckBoxGroup() {
+
+    }
+
+    @Override
+    public void remove(int index) {
+        super.remove(index);
+        list.remove(index);
+    }
+
+    @Override
+    public Component add(Component comp) {
+        if (!(comp instanceof JCheckBox))
+            throw new IllegalArgumentException("Component is not a JCheckBox.");
+        super.add(comp);
+        list.add((JCheckBox) comp);
+        return comp;
+    }
+
+    public JCheckBox[] getSelected() {
+        return list.stream().filter(JCheckBox::isSelected).toArray(JCheckBox[]::new);
+    }
+
+    public String[] getSelectedValues() {
+        return list.stream().filter(JCheckBox::isSelected).map(JCheckBox::getText).toArray(String[]::new);
+    }
+
+}
