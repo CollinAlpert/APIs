b5abe753acd4d8688a63351c301b592e5b58debc
collin <collinalpert@gmail.com>
2018-06-06 00:28:26 +0200

    Improved functionality of ObservableList and added method to DBConnection.

--- de/collin/ObservableList.java
+++ de/collin/ObservableList.java
@@ -6,57 +6,86 @@ import java.util.function.Consumer;
 
 public class ObservableList<T> extends ArrayList<T> {
 
-    private Consumer<T> addListener;
-    private Consumer<T> removeListener;
+	private Consumer<T> addListener;
+	private Consumer<T> removeListener;
 
-    public ObservableList() {
-        addListener = null;
-        removeListener = null;
-    }
+	public ObservableList() {
+		addListener = null;
+		removeListener = null;
+	}
 
-    @SafeVarargs
-    public ObservableList(T... elements) {
-        addListener = null;
-        removeListener = null;
-        Collections.addAll(this, elements);
-    }
+	@SafeVarargs
+	public ObservableList(T... elements) {
+		addListener = null;
+		removeListener = null;
+		Collections.addAll(this, elements);
+	}
 
-    public ObservableList(ArrayList<T> elements) {
-        addListener = null;
-        removeListener = null;
-        this.addAll(elements);
-    }
+	public ObservableList(ArrayList<T> elements) {
+		addListener = null;
+		removeListener = null;
+		this.addAll(elements);
+	}
 
-    @Override
-    public boolean add(T element) {
-        super.add(element);
-        if (addListener != null)
-            addListener.accept(element);
-        return true;
-    }
+	@Override
+	public boolean add(T element) {
+		super.add(element);
+		if (addListener != null)
+			addListener.accept(element);
+		return true;
+	}
 
-    @Override
-    public T remove(int index) {
-        T returnValue = this.get(index);
-        if (removeListener != null)
-            removeListener.accept(returnValue);
-        super.remove(index);
-        return returnValue;
-    }
+	@Override
+	public T remove(int index) {
+		T returnValue = this.get(index);
+		if (removeListener != null)
+			removeListener.accept(returnValue);
+		super.remove(index);
+		return returnValue;
+	}
 
-    public void setAddListener(Consumer<T> addListener) {
-        this.addListener = addListener;
-    }
 
-    public void setRemoveListener(Consumer<T> removeListener) {
-        this.removeListener = removeListener;
-    }
+	@Override
+	public boolean remove(Object o) {
+		T returnValue = this.get(indexOf(o));
+		if (removeListener != null)
+			removeListener.accept(returnValue);
+		return super.remove(o);
+	}
 
-    public Consumer<T> getAddListener() {
-        return addListener;
-    }
+	public Consumer<T> getAddListener() {
+		return addListener;
+	}
 
-    public Consumer<T> getRemoveListener() {
-        return removeListener;
-    }
+	public void setAddListener(Consumer<T> addListener) {
+		this.addListener = addListener;
+	}
+
+	public Consumer<T> getRemoveListener() {
+		return removeListener;
+	}
+
+	public void setRemoveListener(Consumer<T> removeListener) {
+		this.removeListener = removeListener;
+	}
+
+	public void addAddListener(Consumer<T> addListener) {
+		if (this.addListener == null)
+			this.addListener = addListener;
+		else this.addListener.andThen(addListener);
+	}
+
+	public void addRemoveListener(Consumer<T> removeListener) {
+		if (this.removeListener == null)
+			this.removeListener = removeListener;
+		else this.removeListener.andThen(removeListener);
+	}
+
+	public void removeAddListener() {
+		this.addListener = null;
+	}
+
+	public void removeRemoveListener() {
+		this.removeListener = null;
+	}
 }
